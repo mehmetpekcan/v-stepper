6549d14e0868af8b21e8ab944aa9dcb8180b9bbc
Mehmet Pekcan <mehmetpekcannn@gmail.com>
2020-08-22 03:31:09 +0300

    Initialized

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <div style="height: 100vh; display: flex; align-items: center; justify-content: center;">
+      <v-steps></v-steps>
+    </div>
+  </div>
+</template>
+
+<script>
+import VSteps from "@/components/VSteps"
+
+export default {
+  name: 'App',
+  components: { VSteps }
+}
+</script>
---
+++ src/components/VSteps.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="vsteps--wrapper">
+      <div class="vsteps--bar" v-for="(item, key) in 5" :key="key">
+        <div class="bar--top" :class="{ 'active': key < 3 }">
+          <span class="bar--top-step">{{ key + 1 }}</span>
+        </div>
+        <div class="bar--title">
+          <p>Title {{ key + 1 }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import "./main.scss";
+</style>
